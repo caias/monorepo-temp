b8f0cffaa498a413f20fd106f46ce96f90df1553
이강원 <caias1018@gmail.com>
2024-04-24 14:13:06 +0900

    :sparkle: env logic수정 및 npm script 변경

--- apps/next-latest/next.config.mjs
+++ apps/next-latest/next.config.mjs
@@ -3,14 +3,13 @@ import path from 'node:path';
 
 const stage = process.env.STAGE || 'dev';
 const cwd = process.cwd();
-const env = config({
-  path: path.join(cwd, 'envs', `.env.${stage}`),
-}).parsed;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env,
+  env: config({
+    path: path.join(cwd, 'envs', `.env.${stage}`),
+  }).parsed,
 };
 
 export default nextConfig;
